941d1e1777bd00cdcf50c941bc8809247b0db29a
JaimeLHF <102550891+JaimeLHF@users.noreply.github.com>
2022-08-19 14:32:25 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,60 +1,60 @@
 var config = {
-    apiKey: "AIzaSyCdNN5XeUaLOXq8SpscugpsywxsJgcYEtA",
-    authDomain: "teste005-18c6c.firebaseapp.com",
-    projectId: "teste005-18c6c",
-    storageBucket: "teste005-18c6c.appspot.com",
-    messagingSenderId: "766477992738",
-    appId: "1:766477992738:web:d8585841f1409fb5e8db7b",
-    measurementId: "G-LHGLVXZ3FL"
-  };
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////Your script from firebase///////
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+};
 
-firebase.initializeApp(config);
-
-function ligar() {
-		
-    var firebaseRef = firebase.database().ref().child("Led1Status");  
-    
-   
-        firebaseRef.set("1"); 
-            
-        
+firebase.initializeApp(config); ////initialize app from firebase
 
 
+
+
+///////////////////////JS////////////////////////////////////////////////
+
+
+
+
+/////////////////Turn on/////////////////////////////////////////////////
+
+function on() {		
+    var firebaseRef = firebase.database().ref().child("Led1Status");
+
+        firebaseRef.set("1");                 
 }
 
-function desligar() {
-		
+
+
+/////////////////Turn off/////////////////////////////////////////////////
+
+function off() {		
     var firebaseRef = firebase.database().ref().child("Led1Status");
 
         firebaseRef.set("0");
-
 }
 
 
 
+////////////////Functions for hide/show button////////////////////////////
 
-var buttonLigar = document.getElementById("ligar");
-var buttonOff = document.getElementById("desligar");
 
-buttonLigar.addEventListener('click', function(){
-   
-        buttonLigar.style.display = "none";
-      
+var buttonOn = document.getElementById("on");
+var buttonOff = document.getElementById("off");
+
+
+buttonOn.addEventListener('click', function(){
+
+        buttonOn.style.display = "none";      
         buttonOff.style.display = "block";  
-     
 })
 
 
 buttonOff.addEventListener('click', function(){
-   
-    buttonLigar.style.display = "block";
-    
-    buttonOff.style.display = "none";  
 
- 
+    buttonOn.style.display = "block";    
+    buttonOff.style.display = "none"; 
 })
 
-
-function hidden(){
-    
-}
